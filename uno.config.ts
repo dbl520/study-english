cb5f650568ff831d63e7dcdd57ebbfc1c48fdaf6
dbl520 <2239146847@qq.com>
2026-04-22 09:58:46 +0800

    feat: redesign app to match grammar physics sandbox mockup

---
+++ uno.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, presetUno, presetAttributify, presetIcons } from 'unocss'
+
+export default defineConfig({
+  presets: [presetUno(), presetAttributify(), presetIcons()],
+  theme: {
+    colors: {
+      brand: '#2563eb'
+    }
+  }
+})
